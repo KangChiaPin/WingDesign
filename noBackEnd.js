3f12b2a077fa8327df0b5ad97687cae89b5a0e94
Marksein <jeff27855159@hotmail.com>
2014-04-18 15:56:08 +0800

    no-backend for google spreadsheet

---
+++ noBackEnd.js
@@ -0,0 +1,83 @@
+function get_docs ( key )
+{
+	var url = "https://spreadsheets.google.com/feeds/worksheets/" + key + "/public/values";
+	var result = new Array();
+	$.ajax({
+		url: url,
+		context: document.body,
+		dataType: 'xml',
+		async: false,
+		cache: false,
+		error: function( state, err ){ 
+			alert( 'key is accessable! ' + err );
+		},
+		success: function( xml ){ 
+			var worksheet = new Array();
+			var worksheetCounter = 0;
+			var xml = $( xml ).find('entry').each(function(){
+				var linkCounter = 0;
+				worksheet[worksheetCounter++] = $(this).children('link').attr('href');
+				linkCounter++;
+			});
+
+
+			for( var i = 0; i < worksheet.length; i++ ) {
+				result[i] = get_worksheets( worksheet[i] );
+			}
+		}
+	})
+  	return result;
+}
+
+
+function get_worksheets ( url )
+{
+	if( typeof(url) != 'string' ) return;
+
+	var result = new Array;
+
+	$.ajax({
+		url: url,
+		context: document.body,
+		dataType: 'xml',
+		async: false,
+		cache: false,
+		error: function( state, err ){ 
+			alert( 'Make sure worksheet is published! ' + err );
+		},
+		success: function( xml ){ 
+			var rowsCounter = 0;
+			$( xml ).find('entry').each( function(){
+				var rows = new Array();
+				colCounter = 0;
+				$(this).children().each( function(){
+					var data = $(this);
+					if ( data[0].tagName.indexOf('gsx:') == 0 )
+					{
+						rows[colCounter++] = data.text();
+					}
+					else
+						return;
+				});
+				result[rowsCounter++] = rows;
+			});
+			
+		}
+	})
+  	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
