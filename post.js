2d4140b263fe96a3893b06c4521f239bd397e98a
billy5521 <billy5521@gmail.com>
2014-08-20 19:19:23 +0800

    add gss post managing method

--- post.js
+++ post.js
@@ -19,20 +19,28 @@ function renderData(arr){
 		div.className = 'col-md-4';
 		//div.innerHTML += "<div class=\"bar\"></div>"
 
-			for(var l in arr[k]){	
+		var check;
+
+		for(var l in arr[k]){	
 			//	console.log("l:"+l);
-				var title = arr[k][l].title;
-				var content = arr[k][l].content;
-				console.log(title+":"+content);
-				if(title=="title"){
-					div.innerHTML += "<div class=\"bar\">" + content + "</div>" 
-				}	
-				if(title=="content"){
-					div.innerHTML += "<div class=\"content\">"+ content + "</div>";
-				}
-				document.getElementById("container").appendChild(div);	
-				//$(div).hide();
+			var title = arr[k][l].title;
+			var content = arr[k][l].content;
+			console.log(title+":"+content);
+
+			if(title=="title"){
+				div.innerHTML += "<div class=\"bar\">" + content + "</div>" 
+			}	
+			if(title=="content"){
+				div.innerHTML += "<div class=\"content\">"+ content + "</div>";
 			}
+			if(title=="check"){
+				if(content!=0){
+					document.getElementById("container").appendChild(div);	
+					console.log(content);
+				}
+			}
+			//$(div).hide();
+		}
 	}
 }
 
