5a6487e2e7e4ee1326371ae4bb3314f50298e31b
billy5521 <billy5521@gmail.com>
2014-09-11 10:18:20 +0800

    fixes

--- gss.js
+++ gss.js
@@ -1,78 +1,95 @@
-function gss(key){
-	this.key = key;
+function gss(key, formKey){
+  this.key = key;
+  this.formKey = formKey;
+}
+
+
+function postDataToGoogle() {
+  var title = $('#title').val();
+  var content = $('#content').val();
+
+  $.ajax({
+	url: "https://docs.google.com/forms/d/" + this.formKey + "/formResponse",
+	data: {"entry_1932031799": title, 
+	  "entry_1701212135": content 
+	},
+	type: "POST",
+	dataType: "xml",
+
+  });
 }
 
 gss.prototype.get_docs_array = function(){
-	var response = this.get_docs();
-	var array = arrayOf(response[1]);
-//	console.log(array);
-	return array;
+  var response = this.get_docs();
+  var array = arrayOf(response[1]);
+  //	console.log(array);
+  return array;
 }
 
 function arrayOf(hash){
-	var arr = [];
-	for(var k in hash){
-		//		console.log("k:"+k);
-		arr.push(hash2Array(hash[k]));
-	}
+  var arr = [];
+  for(var k in hash){
+	//		console.log("k:"+k);
+	arr.push(hash2Array(hash[k]));
+  }
 
-	return arr;
+  return arr;
 }
 
 function hash2Array( input ){
-	var output = [],item;
-	input = input;
-	for (var type in input) {
-		item = {};
-		item.title = type;
-		item.content = input[type];
-		output.push(item);
-//		console.log(item.title + " : " + item.content);
-	}
-	return output;
+  var output = [],item;
+  input = input;
+  for (var type in input) {
+	item = {};
+	item.title = type;
+	item.content = input[type];
+	output.push(item);
+	//		console.log(item.title + " : " + item.content);
+  }
+  return output;
 }
 
 gss.prototype.get_docs = function() {
-	var key = this.key;
+  var key = this.key;
 
-	var result = [];
-	$.ajax({
-		url: "https://spreadsheets.google.com/feeds/worksheets/" + key + "/public/values", 
-		context: document.body, dataType: 'xml', async: false, cache: false,
-		error: function( state, err ){ alert( 'key is accessible! ' + err ); },
-		success: function( xml ){ 
-			var worksheet = [];
-			var worksheetCounter = 0;
-			var xml = $( xml ).find('entry').each(function(){
-				//var linkCounter = 0;
-				worksheet[worksheetCounter++] = $(this).children('link').attr('href');
-				//linkCounter++;
-			});
-			for( var i = 0; i < worksheet.length; ++i ) { result[i] = get_worksheets( worksheet[i] ); }
-		}
-	})
-	return result;
+  var result = [];
+  $.ajax({
+	url: "https://spreadsheets.google.com/feeds/worksheets/" + key + "/public/values", 
+	context: document.body, dataType: 'xml', async: false, cache: false,
+	error: function( state, err ){ alert( 'key is accessible! ' + err ); },
+	success: function( xml ){ 
+	  var worksheet = [];
+	  var worksheetCounter = 0;
+	  var xml = $( xml ).find('entry').each(function(){
+		//var linkCounter = 0;
+		worksheet[worksheetCounter++] = $(this).children('link').attr('href');
+		//linkCounter++;
+	  });
+	  for( var i = 0; i < worksheet.length; ++i ) { result[i] = get_worksheets( worksheet[i] ); }
+	}
+  })
+  return result;
 }
 
 function get_worksheets ( url ) {
-	if( 'string' != typeof(url) ) return;
-	var result = [];
-	$.ajax({
-		url: url, context: document.body, dataType: 'xml', async: false, cache: false,
-		error: function( state, err ){ alert( 'Make sure worksheet is published! ' + err ); },
-		success: function( xml ){ 
-			var rowsCounter = 0;
-			$( xml ).find('entry').each( function(){
-				var rows = {};
-				var colCounter = 0;
-				$(this).children().each( function(){
-					var data = $(this);
-					if ( 0 == data[0].tagName.indexOf('gsx:') ) { rows[data[0].tagName.substring(4)] = data.text(); }
-				});
-				result[rowsCounter++] = rows;
-			});
-		}
-	});
-//	console.log(result);
-	return result;
+  if( 'string' != typeof(url) ) return;
+  var result = [];
+  $.ajax({
+	url: url, context: document.body, dataType: 'xml', async: false, cache: false,
+	error: function( state, err ){ alert( 'Make sure worksheet is published! ' + err ); },
+	success: function( xml ){ 
+	  var rowsCounter = 0;
+	  $( xml ).find('entry').each( function(){
+		var rows = {};
+		var colCounter = 0;
+		$(this).children().each( function(){
+		  var data = $(this);
+		  if ( 0 == data[0].tagName.indexOf('gsx:') ) { rows[data[0].tagName.substring(4)] = data.text(); }
+		});
+		result[rowsCounter++] = rows;
+	  });
+	}
+  });
+  //	console.log(result);
+  return result;
 }
--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-var DataKey = "0AsPefJI-LLcmdFpCSm53aVZ4OVE5cDgxZ2FqYUtlcnc";
-var gss = new gss(DataKey);
+var dataKey = "0AsPefJI-LLcmdFpCSm53aVZ4OVE5cDgxZ2FqYUtlcnc";
+var formKey = "198xHCsaw2AmUHzeu1TIDw0gbGDpM3CynDuzWsa3NTEE";
+
+
+var gss = new gss(dataKey, formKey);
 
 $(document).ready(function(){
-	renderData(gss.get_docs_array());
+  renderData(gss.get_docs_array());
 });
 
+
 function renderData(arr){
-	for(var k in arr){
-		var id = "data_" + k;
-		var div = document.createElement('div');
-		//console.log(div);
-		div.id = id;
-		div.className = 'col-md-4';
+  for(var k in arr){
+	var div = document.createElement('div');
+	//console.log(div);
+	div.className = 'col-md-4';
 
-		var check;
+	var check;
 
-		for(var l in arr[k]){	
-			//	console.log("l:"+l);
-			var title = arr[k][l].title;
-			var content = arr[k][l].content;
-			console.log(title+":"+content);
-			
-			if(title=="title"){
-				div.innerHTML += "<div class=\"bar\">" + content + "</div>" 
-			}	
-			if(title=="content"){
-				div.innerHTML += "<div class=\"content\">"+ content + "</div>";
-			}
-			if(title=="check"){
-				if(content!=0){
-					document.getElementById("container").appendChild(div);	
-				}
-			}
-			//$(div).hide();
+	for(var l in arr[k]){	
+	  //	console.log("l:"+l);
+	  var title = arr[k][l].title;
+	  var content = arr[k][l].content;
+	  console.log(title+":"+content);
+
+	  if(title=="title"){
+		div.innerHTML += "<div class=\"bar\">" + content + "</div>" 
+	  }	
+	  if(title=="content"){
+		div.innerHTML += "<div class=\"content\">"+ content + "</div>";
+	  }
+	  if(title=="check"){
+		if(content!=0){
+		  document.getElementById("container").appendChild(div);	
 		}
+	  }
+	  //$(div).hide();
 	}
+  }
 }
